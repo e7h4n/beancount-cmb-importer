f4a66dcb42fba868cd5f57eafaacc8684a3426ba
zhangyc <zhangyc@kanyun.com>
2023-03-17 11:00:23 +0800

    init unittest

---
+++ beancmbpdf/importer_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Test cmb pdf importer"""
+
+import unittest
+
+
+class PdfBill(unittest.TestCase):
+    """Test Pdf Bill Import"""
+
+    def test_import_a_single_page_bill(self):
+        """Test documentation goes here."""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
